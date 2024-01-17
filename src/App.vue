c6fe0ddbfdda42f745c51c48e508af8d91b09edb
maicolkutchoukov <maicol.j.95@gmail.com>
2024-01-17 16:08:21 +0100

    Completato esercizio

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+
+
+<script>
+// 1) Importazione AppTitle da components
+import AppTitle from './components/AppTitle.vue'
+export default {
+    data() {
+        return { 
+            title : 'Hello World!' 
+        }
+    },
+    // 2) Dichiarazione AppTitle in components
+    components: {
+        AppTitle
+    }
+};
+      
+</script>
+
+<template>
+    <div>
+      
+        <h1>
+            {{ title }}
+        </h1>
+        <!-- Utilizzo AppTitle -->
+        <AppTitle/>
+  </div>
+</template>
+
+<style scoped>
+h1{
+    text-align: center;
+    font-size: 3rem;
+    color: blue;
+    background-color: red;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data() {
+        return { 
+            title : 'La mia prima app con Vite' 
+        };
+    },
+};
+</script>
+
+<template>
+    <div>
+        <p>
+            {{ title }}
+        </p>
+    </div>
+</template>
+
+<style scoped>
+p{
+    text-align: center;
+    font-size: 2rem;
+    color: lightblue;
+    background-color: black;
+}
+</style>
